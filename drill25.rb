036f888db667ac67a8f222830aa5c1339fb190c3
nininishishishi <121347235+nininishishishi@users.noreply.github.com>
2023-07-02 10:39:52 +0900

    drill26

--- drill25.rb
+++ drill25.rb
@@ -32,5 +32,27 @@ add_result = addition(x, y)
 #  multiplicationメソッドにX,Yを引数として渡し、処理結果を変数multiple_resultに代入する。
 multiple_result = multiplication(x, y)
 
+puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
 
+def addition(a, b)
+  a + b
+end
+
+def multiplication(a,b)
+  a * b
+end
+
+def slice_num(num)
+  # 10の位
+  tens_place = (num / 10) % 10
+  # 1の位
+  ones_place = num % 10
+  return tens_place, ones_place
+end
+
+puts "二桁の整数を入力してください"
+input = gets.to_i
+X, Y = slice_num(input)
+add_result = addition(X, Y)
+multiple_result = multiplication(X, Y)
 puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
---
+++ drill26.rb
@@ -0,0 +1,8 @@
+def extra_end(str)
+  # 処理を記述
+a = str.slice(-2,2)
+puts a * 3
+end
+
+# 呼び出し例
+extra_end('Hello') 
